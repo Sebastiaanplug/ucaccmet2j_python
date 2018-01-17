a99b21193bf0c533cef888d396fa7cb3dd8d36ca
Sebastiaanplug <35219329+Sebastiaanplug@users.noreply.github.com>
2018-01-17 16:45:56 +0100

    Add files via upload
    This is the newest python file. Commit 3

--- python.py
+++ python.py
@@ -8,33 +8,41 @@ Created on Mon Jan  8 12:37:14 2018
 import json
 with open ("precipitation.json") as my_file:
     percip = json.load(my_file)
-    Seattle = [item for item in percip
-                     if item['station'] == 'GHCND:US1WAKG0038']
-    
-    Seattle_total_percipitation = sum(item['value'] for item in seattle_dict)
-    
-    
+   
+daily_Seattle = list()
+for line in percip:
+    Stations = line['station']
+    Date = line['date']
+    Values = line['value']
+    if Stations == 'GHCND:US1WAKG0038': 
+      daily_Seattle.append(Values)
 
+Seattle_total_percipitation = sum(daily_Seattle)  #total yearly rainfall
+print(Seattle_total_percipitation)
+
+monthsinyear = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+Seattle_monthly_percipitation = list()
+for month in monthsinyear:
+    monthly_Seattle = list()
+    for line in percip:
+        Stations = line['station']
+        Date = line['date'][5:7]
+        Values = line['value']
+        if Stations == 'GHCND:US1WAKG0038': 
+            if Date == month:
+                monthly_Seattle.append(Values)
+    Seattle_monthly_percipitation.append(sum(monthly_Seattle))
+
+Seattle_relative_month_percipitation = [x / Seattle_total_percipitation for x in Seattle_monthly_percipitation]   
     
-    Date = [item['date'][5:7] for item in Seattle]
-    Values = [item['value'] for item in Seattle]
-    monthvalue = zip(Date,Values)
-    
-    for k,v in monthvalue:
-        monthy_Seattle[(int(k) - 1)] += v
- 
-        
-                    
-    Relative_month =  [x / Seattle_total_percipitation for x in monthy_Seattle]
-    
-    
-    with open('Seattle.json','w') as file:
-        json.dump({"Seattle": {
-                "totalYearlyPrecipitation" : Seattle_total_percipitation,
-                "totalMonthlyPrecipitation" : monthly_Seattle,
-                "state" : "WA",
-                "relativeMonthlyPrecipitation": Relative_month,
-                "Station" : "GHCND:US1WAKG0038"
-            }
-    },file)
+with open('Seattle.json','w') as file:
+    json.dump({"Seattle": {
+            "totalYearlyPrecipitation" : Seattle_total_percipitation,
+            "totalMonthlyPrecipitation" : Seattle_monthly_percipitation,
+            "state" : "WA",
+            "relativeMonthlyPrecipitation": Seattle_relative_month_percipitation,
+            "Station" : "GHCND:US1WAKG0038"
+        }
+},file)
       
